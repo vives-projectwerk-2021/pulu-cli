fa0d12a4aaa557afbb33b792763abbf07ac17bf8
Thomas Crombez <thomas@trikthom.com>
2021-11-05 15:09:29 +0100

    Added proxy command

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -1,58 +1,45 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	// rootCmd.AddCommand(proxyCmd)
-	// proxyCmd.AddCommand(proxyProductionCmd)
-	// proxyCmd.AddCommand(proxyStagingCmd)
+	rootCmd.AddCommand(proxyCmd)
+	proxyCmd.AddCommand(proxyProductionCmd)
+	proxyCmd.AddCommand(proxyStagingCmd)
 }
 
 var proxyCmd = &cobra.Command{
 	Use:   "proxy",
 	Short: "Creates a proxy server or application-level gateway between localhost and the Pulu Server.",
-	Run: func(cmd *cobra.Command, args []string) {
-		proxyStagingCmd.Run(cmd, args)
-	},
 }
 
 var proxyProductionCmd = &cobra.Command{
 	Use: "production",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("prod")
+		if len(args) == 1 {
+			exeCmd := exec.Command("ssh", append(baseSSHargs, "proxy", "production", args[0])...)
+			exeCmd.Stdout = os.Stdout
+			exeCmd.Stdin = os.Stdin
+			exeCmd.Stderr = os.Stderr
+			exeCmd.Run()
+		}
 	},
 }
 
 var proxyStagingCmd = &cobra.Command{
 	Use: "staging",
 	Run: func(cmd *cobra.Command, args []string) {
-		baseSSHargs = append(baseSSHargs, "proxy staging")
-		exeCmd := exec.Command("ssh", baseSSHargs...)
-		exeCmd.Start()
-
-		sigs := make(chan os.Signal, 1)
-		done := make(chan bool, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-		go func() {
-			sig := <-sigs
-			fmt.Println()
-			fmt.Println(sig)
-			done <- true
-		}()
-
-		fmt.Println("awaiting signal")
-		<-done
-		fmt.Println("exiting")
-		exeCmd.Process.Signal(os.Kill)
-		// exeCmd.Process.Kill()
+		if len(args) == 1 {
+			exeCmd := exec.Command("ssh", append(baseSSHargs, "proxy", "staging", args[0])...)
+			exeCmd.Stdout = os.Stdout
+			exeCmd.Stdin = os.Stdin
+			exeCmd.Stderr = os.Stderr
+			exeCmd.Run()
+		}
 	},
 }
